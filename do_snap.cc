fc6d475473fb648a41567b528736f79fd0fa9dd3
Luciano del Valle <ldelvalleb@gmail.com>
2014-04-24 16:24:52 +0200

    include main do_snap.c to work with gadget snapshots

---
+++ do_snap.cc
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include<fstream>
+#include<string>
+#include <string.h>
+#include <sstream>
+#include <math.h>
+
+#include "laod_snapshot.h"
+#include "bhs.h"
+
+#include "nr3.h"
+#include "sort.h"
+
+#include "enclosed_gas_mass.h" 
+#include "sfr.h"
+#include "kennicutt_schmidt.h"
+
+#define pi 3.141592654 
+#define GRAVITY 6.672e-8
+#define BOLTZMANN 1.3806e-16
+#define PROTONMASS 1.6726e-24
+#define Msun 1.989e33
+#define parsec 3.08567758e18
+#define year 31556926 
+
+int allocate_memory(void);
+
+
+using namespace std; 
+
+int main(int argc, char **argv)
+{
+  //char *basename;
+  char input_fname[200], path[200], *basename;
+  basename=argv[1];
+  
+  int  type, snapshot_number, files;
+  sprintf(path, ".");
+  //sprintf(basename, "snapshot");
+  //  system("mkdir Graficos");
+  
+  double Udist, Uvel, Umasa;
+  //ESTO ES PARA SIMULACION DE GALAXIAS
+  
+  //Udist=3.78e21;  // en cm
+  //Uvel=1.4272e8;  // en cm/s
+  //Umasa=1.154e45; // en gramos
+  
+  int j,DN,N;
+  
+  
+  cout<<"Ingrese el Numero del Pimer Snapshot "<<endl;
+  cin >> j;
+  cout<<"Ingrese el Numero del Ultimo Snapshot "<<endl;
+  cin >> N;
+  cout<<"Ingrese cada cuantos snapshots desea analizar"<<endl;
+  cin >> DN; 
+  
+  cout<<"Ingrese la Unidad de Distancia del Sistema en cm"<<endl;
+  cin>>Udist;
+  
+  cout<<"Ingrese la Unidad de Velocidad del Sistema en cm/s"<<endl;
+  cin>>Uvel;
+  
+  cout<<"Ingrese la Unidad de Masa del sistema en g"<<endl;
+  cin>>Umasa;
+  
+  double Utime;
+  Utime=Udist/Uvel;
+	
+  double Mgas_init, Mstars_init;
+  Mstars_init=Mgas_init=0.0;
+  
+  int ID_BH[2];
+  ID_BH[0]=ID_BH[1]=0;
+  int p=0;
+
+  for(int i=j; i<=N ; i=i+DN){
+    snapshot_number= i;                    /* number of snapshot */
+    files=1;    
+    cout<<"Leyendo Snapshot "<<i<<endl;
+    sprintf(input_fname, "%s/%s_%03d", path, basename, snapshot_number);
+    load_snapshot(input_fname, files);
+    
+    if(i==j){
+      
+      for(int k=0;k<NumPart;k++){ 
+	if(P[k].Type==0){
+	  Mgas_init=Mgas_init+P[k].Mass;
+	}
+	if(P[k].Type==2 || P[k].Type==4){
+	  Mstars_init=Mstars_init+P[k].Mass;
+          
+	}
+	if(P[k].Type==5){
+	  ID_BH[p]=Id[k];
+	  p++;
+	}
+	
+      }
+      
+    }
+    
+    //reordering();  /* call this routine only if your ID's are set properly */
+    BHS(i,Udist,Uvel,Umasa,Mgas_init,ID_BH[0],ID_BH[1]);
+
+
+//    write_Mstar_dens(i,Umasa);
+//    SFR();
+//    data_for_keniccutt_schmidt();
+    enclosed_gas_mass(i,Utime, Udist, Umasa); // this routine has a break !! 
+
+  }
+//  Kennicut_Schmidt(Udist,Uvel,Umasa,N,j,DN);
+}
+
+
+
+
